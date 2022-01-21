229d95369c655698c5770ef846d39b2b23cc791b
princepf <ss.princepf@gmail.com>
2022-01-21 12:57:59 +0530

    first commit

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,18 @@
+import UIKit
+import Flutter
+import Firebase
+import GoogleMaps
+
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAsrpHejAKmaQMqKgv0K0sqBoYIQG4RXE8")
+    FirebaseApp.configure()
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
---
+++ lib/map_screen.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(23.8157, 73.0815),
+    zoom: 12.4746,
+  );
+
+  static const CameraPosition _kLake = CameraPosition(
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      zoom: 19.151926040649414);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        // markers: ,
+        mapType: MapType.hybrid,
+        // markers: _markers,
+        onLongPress: (latlng) {
+          print(latlng.latitude);
+          print(latlng.longitude);
+        },
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: const Text('To the lake!'),
+        icon: const Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
